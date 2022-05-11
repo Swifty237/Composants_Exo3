52a39bb0f2dd64a006cd8a0ee11069191d3157b4
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-11 14:42:50 +0200

    Init and end composants exo3

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.box}>
+        <View style={styles.pictureBox}>
+          <Image style={styles.picture} source={require('./assets/rover.jpg')}/>
+        </View>
+
+        <View style={styles.textBox}>
+          <Text style={styles.title}>Title: Michael FARADAY</Text>
+          <Text style={styles.description}>Description: Michael Faraday (Newington, 22 septembre 1791 - Hampton Court, 25 août 1867) est un physicien et un chimiste britannique, connu pour ses travaux fondamentaux dans le domaine de l'électromagnétisme, l'électrochimie, le diamagnétisme, et l'électrolyse. </Text>
+        </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10
   },
+  
+  box: {
+    height: 300,
+    flexDirection: "row",
+    marginTop: 10  
+  },
+
+  picture: {
+    width: 250,
+    borderColor: 'black',
+    borderRadius: 15,
+    borderWidth: 3
+  },
+
+  title: {
+    margin: 10,
+    color: "red",
+    fontSize: 20
+  },
+
+  description: {
+    margin: 10,
+    color: "red",
+    fontSize: 20
+  },
+
+  textBox: {
+    width: 100,
+  },
+
+  pictureBox: {
+    
+  }
 });
